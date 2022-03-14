753296af697163720239cdb98e47683eea903bb6
DawnMagnet <axccjqh@qq.com>
2022-03-14 11:49:08 +0800

    初始化PyO3

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,9 @@
 use pyo3::prelude::*;
 pub mod structs;
 pub mod book_store_info;
-#[pyclass]
-struct Number(i32);
-
-#[pymethods]
-impl Number {
-    #[new]
-    fn new(value: i32) -> Self {
-        Self(value)
-    }
-}
 
 #[pymodule]
 fn school_library(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Number>()?;
     m.add_class::<book_store_info::BookStoreInfo>()?;
     Ok(())
 }
